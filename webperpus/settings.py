b98225a188f3d8fe0f5fe7623bbc9fd37ae7c40a
ridoo <ridomuhyi@gamil.com>
2026-06-17 09:55:37 +0700

    Deployment WebSekolah

--- webperpus/settings.py
+++ webperpus/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6w^5o__o22w!$#rb!pa5vs!+ghtz2n5mah0wyhdwf9crtp+bj8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -73,16 +73,14 @@ WSGI_APPLICATION = 'webperpus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'perpus_db',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
